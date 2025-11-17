ad42fb2531e7507ecfd38f3607b8e763183b1f97
Kostas <kostasvl66@gmail.com>
2025-11-17 20:50:27 +0200

    Create initial implementation of Open and Close functions

--- src/bplus_file_funcs.c
+++ src/bplus_file_funcs.c
@@ -1,41 +1,74 @@
 #include "bplus_file_funcs.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define CALL_BF(call)         \
-  {                           \
-    BF_ErrorCode code = call; \
-    if (code != BF_OK)        \
-    {                         \
-      BF_PrintError(code);    \
-      return bplus_ERROR;     \
-    }                         \
-  }
+#define bplus_ERROR -1
 
+#define CALL_BF(call)             \
+    {                             \
+        BF_ErrorCode code = call; \
+        if (code != BF_OK) {      \
+            BF_PrintError(code);  \
+            return bplus_ERROR;   \
+        }                         \
+    }
 
-int bplus_create_file(const TableSchema *schema, const char *fileName)
-{
-  return -1;
+int bplus_create_file(const TableSchema *schema, const char *fileName) {
+    return -1;
 }
 
+int bplus_open_file(const char *fileName, int *file_desc, BPlusMeta **metadata) {
 
-int bplus_open_file(const char *fileName, int *file_desc, BPlusMeta **metadata)
-{
-  return -1;
+    BF_Block *header_block;
+
+    // Opening B+_Tree File
+    CALL_BF(BF_OpenFile(fileName, file_desc));
+
+    // Receiving data from block 0(where the metadata is stored)
+    BF_Block_Init(&header_block);
+    CALL_BF(BF_GetBlock(*file_desc, 0, header_block));
+    char *header_data = BF_Block_GetData(header_block);
+
+    // Copying metadata from block 0 to the given pointer
+    *metadata = malloc(sizeof(BPlusMeta));
+    if (!(*metadata)) {
+        return -1;
+    }
+    memcpy(*metadata, header_data, sizeof(BPlusMeta));
+    // The pointer receives a *copy* of the actual metadata, it does not point to block 0 itself
+    // Therefore, the metadata in block 0 remains untouched and independent from any other function calls
+    // This is done as a security measure against metadata corruption, or errors caused by copying unaligned data
+    // This *copy* is always updated when the B+_Tree File is opened with this function, so there are no problems with data consistency
+
+    // Clearing memory and setting dangling pointers to NULL
+    CALL_BF(BF_UnpinBlock(header_block));
+    BF_Block_Destroy(&header_block);
+    header_block = NULL;
+
+    free(header_data);
+    header_data = NULL;
+
+    return 0;
 }
 
-int bplus_close_file(const int file_desc, BPlusMeta* metadata)
-{
-  return -1;
+int bplus_close_file(const int file_desc, BPlusMeta *metadata) {
+
+    CALL_BF(BF_CloseFile(file_desc));
+
+    // Since the metadata pointer was used with a *copy* of block 0, which was independent of the Block File Structure,
+    // the pointer is freed and set to NULL to avoid issues with memory allocation and pointer dangling
+    free(metadata);
+    metadata = NULL;
+
+    return 0;
 }
 
-int bplus_record_insert(const int file_desc, BPlusMeta *metadata, const Record *record)
-{
-  return -1;
+int bplus_record_insert(const int file_desc, BPlusMeta *metadata, const Record *record) {
+    return -1;
 }
 
-int bplus_record_find(const int file_desc, const BPlusMeta *metadata, const int key, Record** out_record)
-{  
-  *out_record=NULL;
-  return -1;
+int bplus_record_find(const int file_desc, const BPlusMeta *metadata, const int key, Record **out_record) {
+    *out_record = NULL;
+    return -1;
 }
-
