38c4bb62859a0c9d26e25253ea3b249bdc642d1f
mtozn <altgithub0x00@gmail.com>
2025-11-18 14:39:23 +0200

    Starting bplus_create_file

--- include/bplus_file_structs.h
+++ include/bplus_file_structs.h
@@ -11,6 +11,11 @@
 #include "record.h"
 
 typedef struct {
+    char magic_num[4]; // identifies the file format
+    int block_count; // total number of blocks in the file
+    int record_count; // total number of records in the file
+    int max_records_per_block; // maximum number of records in a data block
+    TableSchema schema; // info for the stored schema (includes record size)
 } BPlusMeta;
 
 #endif // BPLUS_BPLUS_FILE_STRUCTS_H
--- src/bplus_file_funcs.c
+++ src/bplus_file_funcs.c
@@ -11,6 +11,59 @@
     }                         \
   }
 
+// int HeapFile_Create(const char *fileName) {
+
+//     BF_Block *header_block;       /* block 0 that contains the file header */
+//     BF_Block *first_record_block; /* block 1 that is ready to receive records */
+//     BF_Block_Init(&header_block);
+//     BF_Block_Init(&first_record_block);
+
+//     /* creating the file */
+//     CALL_BF(BF_CreateFile(fileName));
+
+//     /* opening to initialize the file */
+//     int file_handle;
+//     CALL_BF(BF_OpenFile(fileName, &file_handle));
+
+//     /* allocating blocks 0 and 1 and writing the header */
+//     CALL_BF(BF_AllocateBlock(file_handle, header_block));
+//     CALL_BF(BF_AllocateBlock(file_handle, first_record_block));
+
+//     HeapFileHeader *header_temp = malloc(sizeof(HeapFileHeader));
+//     if (!header_temp)
+//         return 0;
+//     header_temp->block_count = 2; /* header_block and first_record_block */
+//     header_temp->record_count = 0;
+//     header_temp->records_per_block = (int)((BF_BLOCK_SIZE - METADATA_SIZE) / sizeof(Record));
+//     header_temp->record_size = sizeof(Record);
+//     header_temp->last_block_append_offset = 0;
+
+//     memcpy(BF_Block_GetData(header_block), header_temp, sizeof(HeapFileHeader)); /* memcpy to avoid unaligned address problems */
+//     free(header_temp);
+
+//     /* initializing the first_record_block with its metadata at the end;
+//        the metadata for each block is only an int record_count */
+//     char *record_block_start = BF_Block_GetData(first_record_block);
+
+//     int record_count = 0;                                                     /* 0 records initially */
+//     memcpy(record_block_start + METADATA_OFFSET, &record_count, sizeof(int)); /* memcpy to avoid unaligned address problems */
+
+//     /* setting the blocks dirty, unpinning and destroying */
+//     BF_Block_SetDirty(header_block);
+//     CALL_BF(BF_UnpinBlock(header_block));
+
+//     BF_Block_SetDirty(first_record_block);
+//     CALL_BF(BF_UnpinBlock(first_record_block));
+
+//     /* closing the file */
+//     BF_CloseFile(file_handle);
+
+//     /* memory cleanup */
+//     BF_Block_Destroy(&header_block);
+//     BF_Block_Destroy(&first_record_block);
+
+//     return 1;
+// }
 
 int bplus_create_file(const TableSchema *schema, const char *fileName)
 {
