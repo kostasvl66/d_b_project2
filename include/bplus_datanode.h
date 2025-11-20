31dc095e78d4918acbd3dd205517f871cc519d89
mtozn <altgithub0x00@gmail.com>
2025-11-20 17:06:22 +0200

    added explanations for structs

--- include/bplus_datanode.h
+++ include/bplus_datanode.h
@@ -3,6 +3,17 @@
 /* Στο αντίστοιχο αρχείο .h μπορείτε να δηλώσετε τις συναρτήσεις
  * και τις δομές δεδομένων που σχετίζονται με τους Κόμβους Δεδομένων.*/
 
+/* The structure of a data block is the following; for each [][] pair there is no padding between
+** (START)[int][DataNodeHeader][int[max_records_per_block]][Record][Record]...[Record][possibly unused space](END)
+** - int is 0 for data block, 1 for index block
+** - DataNodeHeader is the data block header
+** - int[max_records_per_block] is an array of indexes to records, remains sorted so that the records themselves need not be sorted;
+**                              Only the first n values are valid, if n is the current number of records in the block
+** - Record (0) ... Record (k) with k < max_records_per_block are record data, each new one appended at the end;
+**                             their ordering is maintained by the index array, which is always updated
+** - possibly unused space is either space not yet used by future records or a remainder < sizeof(Record)
+*/
+
 typedef struct {
     int record_count; // number of records currently stored in the data blocks
     int parent_index; // index of the parent block (index node)
--- include/bplus_index_node.h
+++ include/bplus_index_node.h
@@ -3,6 +3,18 @@
 /* Στο αντίστοιχο αρχείο .h μπορείτε να δηλώσετε τις συναρτήσεις
  * και τις δομές δεδομένων που σχετίζονται με τους Κόμβους Δεδομένων.*/
 
+/* The structure of an index block is the following; for each [][] pair there is no padding between
+** (START)[int][IndexNodeHeader][int][IndexNodeEntry][IndexNodeEntry]...[IndexNodeEntry][possibly unused space](END)
+** - int (first) is 0 for data block, 1 for index block
+** - IndexNodeHeader is the index block header
+** - int (second) is leftmost index of the block, such that for each key accessible via that index: key < first_entry.key,
+**                                                where first_entry is the leftmost (smallest) IndexNodeEntry in the block
+** - IndexNodeEntry (0) ... IndexNodeEntry (k) with k < max_indexes_per_block are key-index entries,
+**                                             such that for each key accessible via entry.index, key >= entry.key for any entry;
+**                                             when a new one is inserted, some others are shifted to maintain ordering
+** - possibly unused space is either space not yet used by future entries or a remainder < sizeof(IndexNodeEntry)
+*/
+
 typedef struct {
     // number of children indexes currently stored
     // number of keys is always current_index_count - 1
@@ -12,8 +24,8 @@ typedef struct {
 } IndexNodeHeader;
 
 typedef struct {
-    // a key such that currentEntry.right_index keys >= key and prevEntry.right_index keys < key,
-    // where prevEntry is any IndexNodeEntry at the left of currentEntry;
+    // a key such that current_entry.right_index keys >= key and prev_entry.right_index keys < key,
+    // where prev_entry is any IndexNodeEntry at the left of current_entry;
     // the leftmost index of an index node is defined separately of this struct
     int key;
     int right_index;
